a86622c3681ffa839437c6eb47ba9cb0cd767ba9
Ian Grant <ian.conway.grant@gmail.com>
2023-02-23 13:30:33 -0500

    Updated IG_lab3.py from notebook; regenerated plots.

--- lab_3/IG_lab3.py
+++ lab_3/IG_lab3.py
@@ -1,14 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 # # Lab 3
 # ## Ian Grant
 
 import requests, json, numpy, os
 import matplotlib.pyplot as plt
 
+
 #Set working directory
-os.chdir("/home/ian/Documents/geocomp_1/lab_2/output")
+os.chdir("/home/ian/Documents/geocomp_1/lab_3/output")
+
 
 #Import the data
 url = "https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_050_00_5m.json"
+
 
 with requests.get(url) as response:
     data = response.json()
@@ -16,24 +22,26 @@ with requests.get(url) as response:
     print('Number of Counties in US: ' + str(len(data['features'])))
     print('First entry is {}'.format(data['features'][0]['properties']['NAME']))
 
+
 # ### Extracting the 'properties' dictionaries
 # 
 # Based on the results of the analysis presented in Lab 2, we only need to deal with the 'properties' dictionaries within 'data'. This is a relatively small part of the data file, since most of the data is geometric coordinates. Therefore, the first step is to make a list of just the 'properties' dictionaries.
 
 counties_props = []
 
+
 for county in data['features']:
     counties_props.append(county['properties'])
 
-#We no longer need to store the whole input file in 'data', but I have left this commented for convenience
-#del(data)
 
+# We no longer need to store the whole input file in 'data', but I have left this commented for convenience
+# del(data)
 
 # ### Aggregating the counties
 # 
 # First, I define a function called aggDict() that takes a list of dictionaries that have at least one key in common. It returns a new dictionary where the keys are all the unique values of the selected common key. 
 # 
-# For example, if we use aggDict() on the list of counties dictionaries using the 'STATE' key, the function will return a dict with one key for each state name. The valueof each key will be a list of all the counties in that state (see the analysis by state below).
+# For example, if we use aggDict() on the list of counties dictionaries using the 'STATE' key, the function will return a dict with one key for each state name. The value of each key will be a list of all the counties in that state (see the analysis by state below).
 
 def aggDict(dict_list, common_key):
     # Aggregate a list of dictionaries by one of their common keys.
@@ -60,6 +68,7 @@ def aggDict(dict_list, common_key):
 
     return agg_dict
 
+
 # ### Calculating the most frequent values
 # 
 # Next, we'll make a function that uses an aggegated dictionary to return a sorted list of the n keys with the most counties.
@@ -71,7 +80,7 @@ def frequentValues(agg_dict, n):
     
     # Arguments:
     # agg_dict -- An aggregated dictionary; see aggDict().
-    # n -- number of keys to return
+    # n -- number of keys to return; -1 returns all keys
     
     # Returns:
     # list[n keys of agg_dict corresponding to the longest values]: A list of keys  sorted in descending order by length of value list corresponding to each key. 
@@ -99,8 +108,11 @@ def frequentValues(agg_dict, n):
     
     return sorted_frequent_values_keys
 
+
+# Next, let's make a function to visually display the n most frequent keys in an aggregated dictionary.
+
 # Plot an aggregated dictionary with key values on x-axis and frequencies on y-axis
-def aggDictPlot(agg_dict, x_title, n=10, save=False, xAlias=lambda x :
+def aggDictPlot(agg_dict, x_title, n=10, save=False, xAlias=lambda x : x):
     # Shows (and optionally saves) the frequency of occurences in an aggregated dict.
                 
     # Arguments:
@@ -112,19 +124,19 @@ def aggDictPlot(agg_dict, x_title, n=10, save=False, xAlias=lambda x :
                 
     plt.close() #closes previous figures if any are open; avoids data from multiple function calls being combined into single figure, but maybe there's a better way
     
-    #subset is true if n is set to plot only a subset of the key values in agg_dict
-    subset = n < len(agg_dict)
+    #isSubset is true if n is set to plot only a subset of the key values in agg_dict
+    isSubset = n < len(agg_dict)
     
-    if subset:
+    if isSubset:
         keys_to_plot = frequentValues(agg_dict, n)
-    else:
+    else: 
         #returns all keys sorted by frequency
         keys_to_plot = frequentValues(agg_dict, -1)
         
     frequencies = [len(agg_dict[key]) for key in keys_to_plot]
 
-    #set title of plot;
-    if subset:
+    #set title of plot
+    if isSubset:
         title = f"Top {n} Most Frequent Values of {titleLabelStyle(x_title)}"
     else:
         title = f"Frequency of All Unique Values of {titleLabelStyle(x_title)}"
@@ -134,23 +146,31 @@ def aggDictPlot(agg_dict, x_title, n=10, save=False, xAlias=lambda x :
     #make plot
     plt.bar(keys_to_plot_alias, frequencies)
     plt.title(title)
-    plt.xlabel(x_title)
+    plt.xlabel(x_title.title()) #Title case for x title
     plt.xticks(rotation=45, ha='right')
     plt.ylabel("Frequency")
 
     #save or show plot
     if save:
-        filename = title.replace(" ", "_")
+        #replace spaces and slashes for filename
+        filename = filenameStyle(title)
         plt.savefig(f"{filename}.png", bbox_inches="tight", pad_inches=.5)
     else:
         plt.show()
+
 
 #Capitalization rules for the plot title                
 def titleLabelStyle(title_label):
     if title_label.isupper():
         return title_label
     else:
-        return title_label.capitalize()
+        return title_label.title()
+
+
+#Formatting rules for filename
+def filenameStyle(title_label):
+    return title_label.title().replace(" ", "_").replace("/", "-").replace("\\","-")
+
 
 # ### State codes dictionary
 # 
@@ -209,20 +229,25 @@ state_codes_def = """01,Alabama
 56,Wyoming
 72,Puerto Rico"""
 
+
 #Parse defintion string into 2D list of format [[code1, state1], [code2, state2], etc.]
 state_codes_def = state_codes_def.split('\n')
 for i in range(len(state_codes_def)):
     state_codes_def[i] = state_codes_def[i].split(',')
+
 
 #Create state codes dictionary
 state_codes = {}
 for pair in state_codes_def:
     state_codes[pair[0]] = pair[1]
 
+
 # We can also make a function that will take the state code and return the state.
-# This will come in handy for plots graphs later.
+# This will come in handy for plots later.
+
 def stateName(key):
     return state_codes[key]
+
 
 # ## County names analysis
 # 
@@ -232,6 +257,7 @@ def stateName(key):
 
 county_dict = aggDict(counties_props, 'NAME')
 common_county_names = frequentValues(county_dict, 3)
+
 
 for county_name in common_county_names:
     print(f"There are {len(county_dict[county_name])} {county_name} Counties.")
@@ -251,10 +277,12 @@ for county_name in common_county_names:
 
 ans = []
 
+
 for county in counties_props:
     #print(county['NAME'])
     if county['NAME'] == 'Franklin' and county['STATE'] == '51':
         ans.append(county)
+
 
 for county in ans:
     print(county)
@@ -282,12 +310,15 @@ for LSAD in LSAD_dict:
 #Create dictionary aggregated by state code
 state_dict = aggDict(counties_props, 'STATE')
 
+
 print(f"The five states with the most counties are: {[state_codes[state] for state in frequentValues(state_dict, 5)]})")
+
 
 # Next, we can use the state dictionary to calculate state statistics, writing the results to an output file.
 
 #open the output file for writing (which clears the existing data in the file)
 output_file = open("counties_analysis_by_state.txt", "w")
+
 
 for state_code in frequentValues(state_dict, -1): #with n=-1, frequentValues() gives a list of all the  keys sorted by frequency
 
@@ -311,28 +342,42 @@ for state_code in frequentValues(state_dict, -1): #with n=-1, frequentValues() g
     output_file.write(f"The smallest is {smallest_county['NAME']} County, with an area of {smallest_county['CENSUSAREA']}.\n")
     output_file.write(f"The largest is {biggest_county['NAME']} County, with an area of {biggest_county['CENSUSAREA']}.\n\n")
 
+
 #close the output file
 output_file.close()
+
 
 #open the output file again for reading
 output_file = open("counties_analysis_by_state.txt", "r")
 
+
 #print the first 1000 characters of the output file
 print(output_file.read(1000))
 
+
 output_file.close()
+
 
 # ### Plotting the aggregated dictionaries
 # Finally, we can use the aggDictPlot() function to take a look at dictionaries we've made so far. 
 # First, let's look at the top 20 most frequent county names.
-# Note that the plotting  function properly capitalizes 'county' even though it is lowercase in the function call.                
-                
+# Note that the plotting  function properly capitalizes 'county' even though it is lowercase in the function call.         
+
 aggDictPlot(county_dict, 'county', save=True, n=20)
 
+
 # Next, we can do the same for states, using the default number of elements to plot and the stateName function to get the state names from the state codes. 
+# 
+
 aggDictPlot(state_dict, 'State', save=True, xAlias=stateName)
 
+
 # Finally, in the case of the LSAD function, the plot can tell that all of the elements are plotted, so the title changes accordingly.
-aggDictPlot(county_dict, 'LSAD', save=True)
+# 
+# Also note that the plotting function corrects the lowercase "a" in the function call for "area" below.
+# 
+
+aggDictPlot(LSAD_dict, 'Legal/Statistical area Description', save=True)
 
 
+# These three plots can also be found as .png files in the working directory.
